a6f2cc47ecdef8b9b0f514cc08c216f21ca89c0f
Q-PC\quana <quanhpublic@gmail.com>
2026-02-16 19:29:29 +0100

    added hero section + typewriter effect

---
+++ script/hero.js
@@ -0,0 +1,37 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ["Front-End Webdeveloper", "3D/2D Animator", "Video Editor"];
+const element = document.getElementById("typewriter");
+
+let sleepTime = 100;
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      element.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      element.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
